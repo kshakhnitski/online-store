d8d8c6a0b27ea41066994126fc24f4c6237dd9ca
Kirill Shakhnitsky <kirill.shakhnitsky@digitalchief.ru>
2024-01-15 01:50:06 +0300

    Added HikariCP to perform DB queries

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -8,6 +8,7 @@ val exposed_version: String by project
 val h2_version: String by project
 val konform_version: String by project
 val postgresql_driver_version: String by project
+val hikari_cp_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.22"
@@ -42,6 +43,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("com.zaxxer:HikariCP:$hikari_cp_version")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
--- catalog/src/main/kotlin/by/kshakhnitski/onelinestore/plugin/Databases.kt
+++ catalog/src/main/kotlin/by/kshakhnitski/onelinestore/plugin/Databases.kt
@@ -2,6 +2,8 @@ package by.kshakhnitski.onelinestore.plugin
 
 import by.kshakhnitski.onelinestore.model.Categories
 import by.kshakhnitski.onelinestore.model.Products
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -12,12 +14,19 @@ object DatabaseSingleton {
 }
 
 fun Application.configureDatabases() {
-    val database = Database.connect(
-        url = environment.config.property("database.url").getString(),
-        driver = environment.config.property("database.driver").getString(),
-        user = environment.config.property("database.user").getString(),
+    val config = HikariConfig().apply {
+        jdbcUrl = environment.config.property("database.url").getString()
+        driverClassName = environment.config.property("database.driver").getString()
+        username = environment.config.property("database.user").getString()
         password = environment.config.property("database.password").getString()
-    )
+        maximumPoolSize = 10
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    }
+    val datasource = HikariDataSource(config)
+
+    val database = Database.connect(datasource)
     DatabaseSingleton.database = database
 
     transaction(database) {
