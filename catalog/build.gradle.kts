6e2a5cd3389c971fa4051933b26a57325a792b23
Kirill Shakhnitsky <kirill.shakhnitsky@digitalchief.ru>
2024-01-15 01:38:05 +0300

    H2 database has been replaced by PostgreSQL

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("PropertyName")
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -5,6 +7,8 @@ val logback_version: String by project
 val exposed_version: String by project
 val h2_version: String by project
 val konform_version: String by project
+val postgresql_driver_version: String by project
+
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
@@ -33,11 +37,11 @@ dependencies {
     implementation("io.ktor:ktor-server-status-pages-jvm")
     implementation("io.ktor:ktor-server-cors-jvm")
     implementation("io.ktor:ktor-server-swagger-jvm")
+    implementation("org.postgresql:postgresql:$postgresql_driver_version")
     implementation("io.konform:konform-jvm:$konform_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
