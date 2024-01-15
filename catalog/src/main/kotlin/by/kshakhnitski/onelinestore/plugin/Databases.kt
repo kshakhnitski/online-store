93874e753e830e0f77469ddc3eaa8553a327dffa
Kirill Shakhnitsky <kirill.shakhnitsky@digitalchief.ru>
2024-01-15 03:23:21 +0300

    Extracted initialization login to DatabaseFactory singleton

--- catalog/src/main/kotlin/by/kshakhnitski/onelinestore/plugin/Databases.kt
+++ catalog/src/main/kotlin/by/kshakhnitski/onelinestore/plugin/Databases.kt
@@ -1,29 +1,8 @@
 package by.kshakhnitski.onelinestore.plugin
 
-import by.kshakhnitski.onelinestore.model.Categories
-import by.kshakhnitski.onelinestore.model.Products
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
+import by.kshakhnitski.onelinestore.DatabaseFactory
 import io.ktor.server.application.*
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
-    val config = HikariConfig().apply {
-        jdbcUrl = environment.config.property("database.url").getString()
-        driverClassName = environment.config.property("database.driver").getString()
-        username = environment.config.property("database.user").getString()
-        password = environment.config.property("database.password").getString()
-        maximumPoolSize = 10
-        isAutoCommit = false
-        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-        validate()
-    }
-    val datasource = HikariDataSource(config)
-    Database.connect(datasource)
-
-    transaction {
-        SchemaUtils.create(Categories, Products)
-    }
+    DatabaseFactory.init()
 }
